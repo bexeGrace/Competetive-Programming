d0423c23a529d681c4ef481a5c9122c3b004ce8d
Betsegaw Mesele [UGR-9758-14] <betsemese@gmail.com>
2024-05-07 11:38:00 +0300

    hill climbing finalized with the use of random restart algorithm

--- Local Searches/8 Queens/hill_climbing.py
+++ Local Searches/8 Queens/hill_climbing.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 class Table:
     def __init__(self, size = 8):
@@ -27,6 +28,28 @@ class Table:
             generated.append((rows[i], cols[i]))
 
         return generated
+    
+    def generate_successors(self, state):
+        successors = []
+
+        for i, (queen_row, queen_col) in enumerate(state):
+            # Create a copy of the current state
+            new_state = copy.deepcopy(state)
+            # Remove the queen from its current position
+            new_state.pop(i)
+            # Try placing the queen in each row of its column
+            for row in range(self.size):
+                # Check if placing the queen in this row is valid
+                if all((row != q_row and
+                        abs(row - q_row) != abs(queen_col - q_col))
+                       for q_row, q_col in new_state):
+                    # If valid, add the new position to the successors
+                    new_state.append((row, queen_col))
+                    successors.append(new_state)
+                    # Create a fresh copy of the state for the next iteration
+                    new_state = copy.deepcopy(state)
+
+        return successors
 
 
 
@@ -38,10 +61,13 @@ class Queens_8:
         rows = set()
         cols = set()
         count = 0
+        # print(state)
         for row, col in state:
             rows.add(row)
             cols.add(col)
             for row2, col2 in state:
+                if row2==row or col==col2:
+                    continue
                 delta_row = abs(row - row2)
                 delta_col = abs(col - col2)
                 if delta_row == delta_col:
@@ -52,39 +78,45 @@ class Queens_8:
         return count
 
 
-    def generate_neghibors(self, state: list):
-        neghibors = []
+
+
+
+    def generate_neighbors(self, state: list):
+        neighbors = []
         for i in range(len(state)):
-            for j in range(len(state)):
-                temp = state.copy()
-                row, col = state[j]
-                temp[j] = (row, (col + 1)%8)
-                if temp not in neghibors:
-                    neghibors.append(temp)
-
-       
-        
-        return neghibors
+            for row in range(8):
+                if row != state[i][0]:
+                    temp = state.copy()
+                    temp[i] = (row, state[i][1])  # Only change the row number
+                    neighbors.append(temp)
+        return neighbors
+    
 
 
-    def hill_climbing(self, state, prev = None):
-        neghibors = self.generate_neghibors(state)
+    def hill_climbing(self, state):
+
+        prev = None
 
         curr = state
-        # print(neghibors)
-        for c_state in neghibors:
-            val = self.objective_function(c_state)
-            if val < self.objective_function(curr):
-                curr = c_state
+        while True:
+            neghibors = self.generate_neighbors(curr)
 
-        if curr == prev:
-            if curr == 0:
-                return curr, "Succesful"
-            return curr, 'Failed', self.objective_function(curr)
+            # print(neghibors)
+            prev = curr
+            for c_state in neghibors:
+                val = self.objective_function(c_state)
+                if val <= self.objective_function(curr):
+                    curr = c_state
+            
+
+            if self.objective_function(curr) == self.objective_function(prev):
+                if self.objective_function(curr) == 0:
+                    return curr, "Succesful"
+                else:
+                    curr = Table.generate_random(Table)
+                    prev = None
+            
         
-        
-        
-        return self.hill_climbing(curr, state)
         
         
             
@@ -94,6 +126,9 @@ class Queens_8:
 tb = Table()
 i_s = tb.generate_random()
 gm = Queens_8(i_s)
+a= gm.objective_function([(1, 5), (6, 1), (4, 4), (0, 2), (2, 7), (7, 3), (3, 0), (5, 6)])
+print(a)
+# print(tb.generate_successors(i_s))
 # print(gm.objective_function(i_s), i_s)
 # print(gm.generate_neghibors(i_s), i_s)
 print(gm.hill_climbing(i_s), i_s)
