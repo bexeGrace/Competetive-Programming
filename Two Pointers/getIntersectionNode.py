8dab4180019ed013be7e6a2cc7ed0082b40d65d3
Betsegaw Mesele <146959506+bexeGrace@users.noreply.github.com>
2025-02-09 08:23:56 +0300

    fixed some issue

--- Two Pointers/getIntersectionNode.py
+++ Two Pointers/getIntersectionNode.py
@@ -12,27 +12,3 @@ def getIntersectionNode(headA: ListNode, headB: ListNode):
         listb = listb.next if listb else headA
     
     return listb
-
-
-A = ListNode(4)
-A2 = ListNode(1)
-A3 = ListNode(8)
-A4 = ListNode(4)
-A5 = ListNode(5)
-A.next = A2
-A2.next = A3
-A3.next = A4
-A4.next = A5
-
-B = ListNode(5)
-B2 = ListNode(6)
-B3 = ListNode(1)
-B4 = ListNode(8)
-B5 = ListNode(4)
-B6 = ListNode(5)
-B.next = B2
-B2.next = B3
-B3.next = A3
-
-
-print(getIntersectionNode(A, B))
