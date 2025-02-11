5d1cff16c2330e64f63bd9734884458679249c2b
Betsegaw Mesele <146959506+bexeGrace@users.noreply.github.com>
2025-02-11 16:26:27 +0300

    longest palindrome

---
+++ Greedy Algorithm/longestPalindrome.py
@@ -0,0 +1,19 @@
+def longestPalindrome(s: str) -> int:
+    setVal = set(s)
+    boo = False
+    o_sum = 0
+    e_sum = 0
+    for i in setVal:
+        if s.count(i) == 1:
+            boo = True
+        elif s.count(i) % 2 != 0:
+            o_sum += s.count(i) - 1
+        else:
+            e_sum += s.count(i)
+    if o_sum > 0 or boo:
+        o_sum += 1
+    return e_sum + o_sum
+    
+
+
+print(longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
