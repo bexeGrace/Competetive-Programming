179d953c9bcdc0e470125fdccef0d5f123ef7ee1
Betsegaw Mesele <146959506+bexeGrace@users.noreply.github.com>
2025-03-24 14:00:21 +0300

    single number one

---
+++ Array/singleNumberI.py
@@ -0,0 +1,9 @@
+from typings import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        XOR = 0
+        for i in nums:
+            XOR = XOR ^ i
+        
+        return XOR
