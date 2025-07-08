9145cd4ad591c7ab0d8a8d0444399b00d5fa7d8a
Betsegaw Mesele <146959506+bexeGrace@users.noreply.github.com>
2025-07-08 14:45:02 +0300

    for array

--- Recursion and Backtracking/mergeTwoSortedLists.py
+++ Recursion and Backtracking/mergeTwoSortedLists.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -18,3 +20,37 @@ class Solution:
         else:
             list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        curr = len(nums1) - 1
+        i, j = m-1, n-1
+        if m == 0 or n == 0:
+            if n > 0:
+                nums1 = nums2
+            else:
+                nums1 = nums1[:m] 
+        else:
+            while i >= 0 or j >= 0:
+                if nums1[i] >= nums2[j]:
+                    nums1[curr] = nums1[i]
+                    i -= 1
+            
+                else:
+                    nums1[curr] = nums2[j]
+                    j -= 1
+                curr -= 1
+                if i < 0 or j < 0:
+                    break
+        
+        print(nums1)
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+sol = Solution()
+sol.merge(nums1, 3, nums2, 3)    
